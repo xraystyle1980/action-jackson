1ed84dade7b928fbb92f5020b4f9ab5fe91917d3
Matt Trice <matt.trice@gmail.com>
2020-04-12 12:07:59 -0400

    trigger

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -7,7 +7,7 @@ const Nav = (props) => (
         <Scrollspy items={ ['intro', 'first', 'second', 'cta'] } currentClassName="is-active" offset={-300}>
             <li>
                 <Scroll type="id" element="intro">
-                    <a href="#">Introduction</a>
+                    <a href="#">Outtro</a>
                 </Scroll>
             </li>
             <li>
