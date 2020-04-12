55aac94e82f84376dd1760d27eea3d54727b4044
Matt Trice <matt.trice@gmail.com>
2020-04-12 19:14:54 -0400

    center it up

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,23 @@
 import { Link } from 'gatsby'
 import React from 'react'
 import Helmet from 'react-helmet'
-import { Waypoint } from 'react-waypoint'
-import pic01 from '../assets/images/pic01.jpg'
 import Header from '../components/Header'
 import Layout from '../components/layout'
-import Nav from '../components/Nav'
+import styled from 'styled-components'
+
+
+const MainSection = styled.section`
+
+`;
+
+const BioContainer = styled.div`
+  padding: 0 12em;
+  text-align: center;
+`;
 
 
 class Index extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      stickyNav: false,
-    }
-  }
+
 
   render() {
     return (
@@ -24,46 +27,37 @@ class Index extends React.Component {
         <Header />
 
         <div>
-          <section>
-                <h2>Matt Trice Design</h2>
-                <p>
-                  Designer in the ATL area.
-                  Pushing pixels for Decent Labs.
-                  Yes, I do UX AND UI. I know.
-                </p>
-                <section>
-
-                  <ul className="icons">
-
-
-                    <li>
-                      <a href="https://codebushi.com" className="icon fa-instagram">
-                        <span className="label">Instagram</span>
-                      </a>
-                    </li>
-                    <li>
-                      <a
-                        href="https://github.com/codebushi/gatsby-starter-stellar"
-                        className="icon fa-github"
-                      >
-                        <span className="label">GitHub</span>
-                      </a>
-                    </li>
-                    <li>
-                      <a href="https://codebushi.com" className="icon fa-dribbble">
-                        <span className="label">Dribbble</span>
-                      </a>
-                    </li>
-                  </ul>
-                </section>
-
-          </section>
-
-
-
-
-
-
+          <MainSection>
+            <BioContainer>
+              <h2>Matt Trice Design</h2>
+              <p>
+                Hello, my name is Matt Trice. I'm a Designer in the ATL area.
+                I'm currently pushing pixels and making really cool things with really cool people at Decent Labs. Yes, I do UX <span style={{ fontWeight: '900' }}>&amp;</span> UI. I know.
+              </p>
+              <section>
+                <ul className="icons">
+                  <li>
+                    <a href="https://www.instagram.com/trice.design/" className="icon fa-instagram">
+                      <span className="label">Instagram</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="https://www.linkedin.com/in/matt-trice-5053b325/"
+                      className="icon fa-linkedin"
+                    >
+                      <span className="label">GitHub</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="https://dribbble.com/trice" className="icon fa-dribbble">
+                      <span className="label">Dribbble</span>
+                    </a>
+                  </li>
+                </ul>
+              </section>
+            </BioContainer>
+          </MainSection>
         </div>
       </Layout>
     )
